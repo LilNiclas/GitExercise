fd43d1ede3839a94a34ef9a8e164aa12d2b4b247
LilNiclas <nicl2995@stud.kea.dk>
2022-09-09 13:46:31 +0200

    Added greet

--- src/SuperGreeter.java
+++ src/SuperGreeter.java
@@ -1,10 +1,22 @@
+import java.util.Scanner;
+
 public class SuperGreeter {
 
     public void start() {
         System.out.println("Det virker");
+
+        System.out.print("Hvad hedder du: ");
+        Scanner scan = new Scanner(System.in);
+        String greet = scan.nextLine();
     }
+
     public static void main(String[] args) {
         SuperGreeter sg = new SuperGreeter();
         sg.start();
     }
+
+
+
+
+
 }
